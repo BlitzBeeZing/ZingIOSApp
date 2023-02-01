3d0d90fa9c7b3522e7f622615df2cafe3129ca3c
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2023-02-02 01:44:20 +0530

    Initial Commit

---
+++ Zing/ZingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZingApp.swift
+//  Zing
+//
+//  Created by Adarsh Shukla on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ZingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
